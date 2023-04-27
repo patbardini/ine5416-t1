256aac2b0e6accfcbb204a69121b547c47f2cc33
tulio.bardini <tulio.bardini@grad.ufsc.br>
2023-04-27 09:54:14 -0300

    Código belíssimo de Backtraking funcionando para o problema em uma matriz 4x4, primeiro passo para a vitória!

--- Main.hs
+++ Main.hs
@@ -1,10 +1,18 @@
 import Matriz
 import Resolvedor
+import Backtracking
 
 main = do
     -- let dimensaoMatriz = getDimensaoMatriz matriz4x4
+
     let matrizValores = matrizResultado
-    print(getPosicaoNumeroValido matriz4x4 matrizValores (0, 1) 1) 
+    -- print(inserirValorMatriz matrizValores (1, 1) 1)
+
+    let (a, b) = backtrancking matriz4x4 matrizValores (0, 0) 4
+    print a
+    print b
+
+    --print(getPosicaoNumeroValido matriz4x4 matrizValores (1, 3) 1) 
     
     -- print(getOperadorAbaixo matriz4x4 (0, 0))
     -- print(getOperadorAcima matriz4x4 (0, 0))
