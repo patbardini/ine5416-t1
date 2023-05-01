f59c086af8485c57e0999e1ee9254ed81f742d5d
Patrícia Bardini Arigoni <arigonipatricia@gmail.com>
2023-04-30 21:18:47 -0300

    corrige bug na função getPosicaoNumeroValido
    * corrige erro de índice
    * novas funções validaOperadores e isOperadorValido abrangem a lógica da validação dos operadores
    * agora getPosicaoNumeroValido apenas chama os métodos necessários para validar a inserção

--- Main.hs
+++ Main.hs
@@ -2,11 +2,9 @@ import Matriz
 import Resolvedor
 
 main = do
-    -- let dimensaoMatriz = getDimensaoMatriz matriz4x4
-    let matrizValores = matrizResultado
-    print(getPosicaoNumeroValido matriz4x4 matrizValores (0, 1) 1) 
-    
-    -- print(getOperadorAbaixo matriz4x4 (0, 0))
-    -- print(getOperadorAcima matriz4x4 (0, 0))
-    -- print(getOperadorAEsquerda matriz4x4 (0, 0))
-    -- print(getOperadorADireita matriz4x4 (0, 0))
+    let matrizValores = matrizResultado 9
+    let matrizOperadores = matriz9x9
+    let tamanhoMatriz = getDimensaoMatriz matrizOperadores
+
+    print(getPosicaoNumeroValido matrizOperadores matrizValores (7, 8) 7)
+
